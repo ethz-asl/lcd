99cf6ca543836879be825dcde89600a548ef7617
Salvadore101 <dominkwalder@gmx.ch>
2017-06-28 11:21:41 +0200

    Included the depth image in the callback.

--- line_detection/src/detect_and_show_3D_node.cc
+++ line_detection/src/detect_and_show_3D_node.cc
@@ -18,7 +18,8 @@
 #include <visualization_msgs/Marker.h>
 
 typedef message_filters::sync_policies::ExactTime<
-    sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::PointCloud2>
+    sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo,
+    sensor_msgs::PointCloud2>
     MySyncPolicy;
 
 class listenAndPublish {
@@ -38,6 +39,7 @@ class listenAndPublish {
     // Three topics published by scenenet_ros_tools (there are more but needed
     // here so far).
     image_sub_.subscribe(node_handle_, "/camera/rgb/image_raw", 1);
+    depth_sub_.subscribe(node_handle_, "/camera/depth/image_raw", 1);
     info_sub_.subscribe(node_handle_, "/camera/rgb/camera_info", 1);
     pc_sub_.subscribe(node_handle_, "/scenenet_node/scene", 1);
   }
@@ -50,13 +52,14 @@ class listenAndPublish {
     // every call of the callback function recieves three messages that have the
     // same timestamp.
     sync_ = new message_filters::Synchronizer<MySyncPolicy>(
-        MySyncPolicy(10), image_sub_, info_sub_, pc_sub_);
+        MySyncPolicy(10), image_sub_, depth_sub_, info_sub_, pc_sub_);
     sync_->registerCallback(boost::bind(
-        &listenAndPublish::pcCallbackPublishOnce, this, _1, _2, _3));
+        &listenAndPublish::pcCallbackPublishOnce, this, _1, _2, _3, _4));
   }
 
   void pcCallbackPublishOnce(
       const sensor_msgs::ImageConstPtr& rosmsg_image,
+      const sensor_msgs::ImageConstPtr& rosmsg_depth,
       const sensor_msgs::CameraInfoConstPtr& camera_info,
       const sensor_msgs::PointCloud2ConstPtr& rosmsg_cloud) {
     // Extract the point cloud from the message.
@@ -65,6 +68,9 @@ class listenAndPublish {
     cv_bridge::CvImageConstPtr cv_img_ptr =
         cv_bridge::toCvShare(rosmsg_image, "rgb8");
     cv_image_ = cv_img_ptr->image;
+    cv_bridge::CvImageConstPtr cv_depth_ptr =
+        cv_bridge::toCvShare(rosmsg_depth);
+    cv_depth_ = cv_depth_ptr->image;
     cvtColor(cv_image_, cv_img_gray_, CV_RGB2GRAY);
 
     line_detection::pclFromSceneNetToMat(pcl_cloud_, 320, 240, cv_cloud_);
@@ -81,9 +87,7 @@ class listenAndPublish {
 
     start = std::chrono::system_clock::now();
     std::vector<cv::Vec<float, 6> > lines3D_temp;
-    // line_detector_.projectLines2Dto3D(lines2D_, cv_cloud_, lines3D_temp);
     line_detector_.project2Dto3DwithPlanes(cv_cloud_, lines2D_, lines3D_temp);
-    // line_detector_.find3DlinesRated(cv_cloud_, lines2D_, lines3D_temp);
     end = std::chrono::system_clock::now();
     elapsed_seconds = end - start;
     ROS_INFO("Projecting to 3D: %f", elapsed_seconds.count());
@@ -94,13 +98,6 @@ class listenAndPublish {
     elapsed_seconds = end - start;
     ROS_INFO("Check for valid lines: %f", elapsed_seconds.count());
 
-    // lines3D_ = lines3D_temp;
-
-    // std::vector<cv::Vec4f> lines2D_temp;
-    // line_detector_.runCheckOn2DLines(cv_cloud_, lines2D_temp, lines2D_);
-    // line_detector_.projectLines2Dto3D(lines2D_, cv_cloud_, lines3D_);
-
-    // line_detector_.find3DlinesByShortest(cv_cloud_, lines2D_, lines3D_);
     marker_3Dlines_.header.frame_id = pcl_cloud_.header.frame_id;
     line_detection::storeLines3DinMarkerMsg(lines3D_, marker_3Dlines_);
     // The timestamp is set to 0 because rviz is not able to find the right
@@ -125,6 +122,7 @@ class listenAndPublish {
   cv::Mat cv_image_;
   cv::Mat cv_img_gray_;
   cv::Mat cv_cloud_;
+  cv::Mat cv_depth_;
   pcl::PointCloud<pcl::PointXYZRGB> pcl_cloud_;
   std::vector<cv::Vec4f> lines2D_;
   std::vector<cv::Vec<float, 6> > lines3D_;
@@ -136,6 +134,7 @@ class listenAndPublish {
   ros::Publisher marker_pub_;
   message_filters::Synchronizer<MySyncPolicy>* sync_;
   message_filters::Subscriber<sensor_msgs::Image> image_sub_;
+  message_filters::Subscriber<sensor_msgs::Image> depth_sub_;
   message_filters::Subscriber<sensor_msgs::CameraInfo> info_sub_;
   message_filters::Subscriber<sensor_msgs::PointCloud2> pc_sub_;
   // To have the line_detection utility.
