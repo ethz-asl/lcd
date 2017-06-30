98bf535593244ffbdbb0225e4262e0123d0d6108
Salvadore101 <dominkwalder@gmx.ch>
2017-06-30 16:38:48 +0200

    Improved kmeans clustering and added rviz display utility.

--- line_clustering/include/line_clustering/line_clustering.h
+++ line_clustering/include/line_clustering/line_clustering.h
@@ -17,6 +17,7 @@ double computeSquareMeanDifferenceLines(const cv::Vec<float, 6>& line1,
 double computeSquareNearestDifferenceLines(const cv::Vec<float, 6>& line1,
                                            const cv::Vec<float, 6>& line2);
 
+// A class that performs clustering of lines with kmeans.
 class KMeansCluster {
  public:
   KMeansCluster();
@@ -25,15 +26,60 @@ class KMeansCluster {
                 unsigned int num_clusters);
 
   void setNumberOfClusters(unsigned int num_clusters);
+  void setLines(const std::vector<cv::Vec<float, 6> >& lines3D);
+  void setLines(const std::vector<line_detection::LineWithPlanes>& lines3D);
+  // Computes the means of the lines that are used to cluster them.
   void computeMeansOfLines();
+  // Performs the clustering.
   void runOnMeansOfLines();
+  // Returns the lines.
+  std::vector<cv::Vec<float, 6> > getLines();
+  // This array contains the labels of the lines.s
   std::vector<int> cluster_idx_;
 
  private:
+  bool lines_set_, k_set_;
   unsigned int K_;
   std::vector<cv::Vec3f> line_means_;
   std::vector<cv::Vec<float, 6> > lines_;
 };
+
+// This class helps publishing several different clusters of lines in different
+// colors, so that they are visualized by rviz.
+// IMPORTANT: This function cannot display more clusters than there are colors
+//            defined in the constructor. If more clusters are given to the
+//            object, only the one with the highest labels are published.
+class DisplayClusters {
+ public:
+  DisplayClusters();
+  // Frame id of the marker message.
+  void setFrameID(const std::string& frame_id);
+
+  // Is used as input for the clusters to the class:
+  // lines3D:   Vector of 3D lines.
+  //
+  // labels:    Vector of equal size as lines3D. Every entry labels the cluster
+  //            the 3D line with the same index belongs to. The lables should be
+  //            continous ((0,1,2,3 -> good), (0,4,8,16 -> bad)), because the
+  //            highest label defines how many clusters are created (in the
+  //            latter case of the example 17 clusters will be created, but only
+  //            4 will contain information).
+  void setClusters(const std::vector<cv::Vec<float, 6> >& lines3D,
+                   const std::vector<int>& labels);
+
+  // This functions advertises the message.
+  void initPublishing(ros::NodeHandle& node_handle);
+  void publish();
+
+ private:
+  bool frame_id_set_, clusters_set_, initialized_;
+  std::vector<visualization_msgs::Marker> marker_lines_;
+  std::vector<std::vector<cv::Vec<float, 6> > > line_clusters_;
+  std::vector<ros::Publisher> pub_;
+  std::string frame_id_;
+  std::vector<cv::Vec3f> colors_;
+};
+
 }  // namespace line_clustering
 
 #include "line_clustering/line_clustering_inl.h"
--- line_clustering/src/line_clustering.cc
+++ line_clustering/src/line_clustering.cc
@@ -1,6 +1,7 @@
 #include "line_clustering/line_clustering.h"
 
 namespace line_clustering {
+
 double computePerpendicularDistanceLines(const cv::Vec<float, 6>& line1,
                                          const cv::Vec<float, 6>& line2) {
   cv::Vec3f start1(line1[0], line1[1], line1[2]);
@@ -40,20 +41,41 @@ double computeSquareNearestDifferenceLines(const cv::Vec<float, 6>& line1,
   return min_dist;
 }
 
+KMeansCluster::KMeansCluster() {
+  lines_set_ = false;
+  k_set_ = false;
+};
 KMeansCluster::KMeansCluster(const std::vector<cv::Vec<float, 6> >& lines3D) {
   lines_ = lines3D;
+  lines_set_ = true;
+  k_set_ = false;
 };
 KMeansCluster::KMeansCluster(const std::vector<cv::Vec<float, 6> >& lines3D,
                              unsigned int num_clusters) {
   lines_ = lines3D;
   K_ = num_clusters;
+  lines_set_ = true;
+  k_set_ = true;
 }
 
 void KMeansCluster::setNumberOfClusters(unsigned int num_clusters) {
   K_ = num_clusters;
+  k_set_ = true;
+}
+void KMeansCluster::setLines(const std::vector<cv::Vec<float, 6> >& lines3D) {
+  lines_ = lines3D;
+  lines_set_ = true;
+}
+void KMeansCluster::setLines(
+    const std::vector<line_detection::LineWithPlanes>& lines3D) {
+  lines_.resize(lines3D.size());
+  for (size_t i = 0; i < lines3D.size(); ++i) lines_.push_back(lines3D[i].line);
+  lines_set_ = true;
 }
 
 void KMeansCluster::computeMeansOfLines() {
+  CHECK(lines_set_)
+      << "You have to set the lines before computing their means.";
   line_means_.clear();
   line_means_.reserve(lines_.size());
   for (size_t i = 0; i < lines_.size(); ++i) {
@@ -64,10 +86,97 @@ void KMeansCluster::computeMeansOfLines() {
 }
 void KMeansCluster::runOnMeansOfLines() {
   if (lines_.size() != line_means_.size()) computeMeansOfLines();
-  cv::kmeans(
-      line_means_, K_, cluster_idx_,
-      cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 10, 1),
-      3, cv::KMEANS_PP_CENTERS);
+  cv::kmeans(line_means_, K_, cluster_idx_,
+             cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT,
+                              100, 0.01),
+             3, cv::KMEANS_PP_CENTERS);
+}
+
+std::vector<cv::Vec<float, 6> > KMeansCluster::getLines() { return lines_; }
+
+DisplayClusters::DisplayClusters() {
+  colors_.push_back({1, 0, 0});
+  colors_.push_back({0, 1, 0});
+  colors_.push_back({0, 0, 1});
+  colors_.push_back({1, 1, 0});
+  colors_.push_back({1, 0, 1});
+  colors_.push_back({0, 1, 1});
+  colors_.push_back({1, 0.5, 0});
+  colors_.push_back({1, 0, 0.5});
+  colors_.push_back({0.5, 1, 0});
+  colors_.push_back({0, 1, 0.5});
+  colors_.push_back({0.5, 0, 1});
+  colors_.push_back({0, 0.5, 1});
+
+  frame_id_set_ = false;
+  clusters_set_ = false;
+  initialized_ = false;
+}
+
+void DisplayClusters::setFrameID(const std::string& frame_id) {
+  frame_id_ = frame_id;
+  frame_id_set_ = true;
+}
+
+void DisplayClusters::setClusters(
+    const std::vector<cv::Vec<float, 6> >& lines3D,
+    const std::vector<int>& labels) {
+  CHECK_EQ(lines3D.size(), labels.size());
+  size_t N = 0;
+  line_clusters_.clear();
+  for (size_t i = 0; i < lines3D.size(); ++i) {
+    // This if clause sets the number of clusters. This works well as long the
+    // clusters are indexed as an array (0,1,2,3). In any other case, it creates
+    // to many clusters (which is not that bad, because empty clusters do not
+    // need a lot of memory nor a lot of time to allocate), but if one label is
+    // higher than the number of colors defined in the constructor (which
+    // defines the number of labels that can be displayed), some clusters might
+    // not be displayed.
+    if (labels[i] >= N) {
+      N = 1 + labels[i];
+      line_clusters_.resize(N);
+    }
+    if (labels[i] < 0) {
+      ROS_WARN_ONCE(
+          "line_clustering::DisplayClusters::setClusters: A negative label has "
+          "been detected and ignored.");
+      continue;
+    }
+    line_clusters_[labels[i]].push_back(lines3D[i]);
+  }
+  marker_lines_.resize(line_clusters_.size());
+  size_t n;
+  for (size_t i = 0; i < line_clusters_.size(); ++i) {
+    n = i % colors_.size();
+    line_detection::storeLines3DinMarkerMsg(line_clusters_[i], marker_lines_[i],
+                                            colors_[n]);
+    marker_lines_[i].header.frame_id = frame_id_;
+  }
+  clusters_set_ = true;
+}
+
+void DisplayClusters::initPublishing(ros::NodeHandle& node_handle) {
+  pub_.resize(colors_.size());
+  size_t n;
+  for (size_t i = 0; i < colors_.size(); ++i) {
+    std::stringstream topic;
+    topic << "/visualization_marker_" << i;
+    pub_[i] =
+        node_handle.advertise<visualization_msgs::Marker>(topic.str(), 1000);
+  }
+  initialized_ = true;
+}
+
+void DisplayClusters::publish() {
+  CHECK(initialized_)
+      << "You need to call initPublishing to advertise before publishing.";
+  CHECK(frame_id_set_) << "You need to set the frame_id before publishing.";
+  CHECK(clusters_set_) << "You need to set the clusters before publishing.";
+  int n;
+  for (size_t i = 0; i < marker_lines_.size(); ++i) {
+    n = i % pub_.size();
+    pub_[n].publish(marker_lines_[i]);
+  }
 }
 
 }  // namespace line_clustering
