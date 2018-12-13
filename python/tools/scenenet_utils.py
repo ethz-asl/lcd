Fix nasty bug in rectangle rotation + slightly change interface
- Fix bug that prevented rectangles from being successfully rotated when
needed in findPointsInRectangle: when rotating CCW a point (x,y) of an
angle theta around the origin, the mapped point (x',y') has y' =
sin(theta)*x + cos(theta)*y instead of cos(theta)*x + sin(theta)*y.
- Change interface of findPointsInRectangle to modify the input
rectangle when rotation is applied (used for debug, but might be as well
be applied to all occurrences in the code). Keep the old interface as
overload.
- Add a counter that for each frame assigns an index to lines that are
successfully projected in 3D (useful to map lines between the detection
and the ground-truth labelling parts).
- Fix minor typos.

python/tools/scenenet_utils.py
@@ -126,7 +126,7 @@ def get_origin_virtual_camera(line, distance, debug=False):
     if line[n] == 0:  # dicontinuty line
         if debug is True:
             print("disconti")
-        # discontinuty line only has one valid surface normal, the other one is [0, 0, 0, 0]
+        # discontinuity line only has one valid surface normal, the other one is [0, 0, 0, 0]
         if np.linalg.norm(plane1_normal) <= 0.001:
             z_cand = [plane2_normal, -plane2_normal]
         if np.linalg.norm(plane2_normal) <= 0.001: